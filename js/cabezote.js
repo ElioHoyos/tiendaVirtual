f3a4d008153d5b8ffaac347bbc29cf260e8bca5c
ElioHoyos <33575595+ElioHoyos@users.noreply.github.com>
2024-10-30 15:16:02 -0500

    js categorias

---
+++ js/cabezote.js
@@ -0,0 +1,8 @@
+// Categorias
+$("#btnCategorias").click(function(){
+    if(window.matchMedia("(max-width:767px)").matches){
+        $("#btnCategorias").after($("#categorias").slideToggle("fast"))
+    }else{
+        $("#cabezote").after($("#categorias").slideToggle("fast"))
+    }
+})
